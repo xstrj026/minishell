2ab1a05da131452723e5480c01fc0c2be6169d80
pepa <pepastrojsa@seznam.cz>
2024-03-12 13:01:16 +0100

    libft add

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jstrojsa <jstrojsa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 04:57:58 by ytsyrend          #+#    #+#             */
+/*   Updated: 2024/01/25 15:47:20 by jstrojsa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_libft_list	*ft_lstmap(t_libft_list *lst, void *(*f)(void *),
+			void (*del)(void *))
+{
+	t_libft_list	*savenode;
+	t_libft_list	*listnew;
+	t_libft_list	*newnode;
+
+	savenode = lst;
+	listnew = NULL;
+	while (savenode)
+	{
+		newnode = ft_lstnew(f(savenode->content));
+		if (newnode == NULL)
+		{
+			ft_lstclear(&listnew, del);
+			return ((void *)(0));
+		}
+		ft_lstadd_back(&listnew, newnode);
+		savenode = savenode->next;
+	}
+	newnode = NULL;
+	return (listnew);
+}
